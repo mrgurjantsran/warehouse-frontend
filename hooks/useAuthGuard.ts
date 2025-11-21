0abbfa50680afb4ce9a0c2f00ce13b9c7b03faba
mrgurjantsran <mr.gurjantsran@gmail.com>
2025-11-21 12:32:15 +0530

    Add files via upload

---
+++ hooks/useAuthGuard.ts
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export const useAuthGuard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.replace("/login");
+    }
+  }, []);
+};
